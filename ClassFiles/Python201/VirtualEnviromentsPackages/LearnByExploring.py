903a7e1d561b586cb6d8e210b6d3f0b7d5576d0c
minefarmer <pgoldfarm@yahoo.com>
2021-02-01 17:27:03 -0600

    cleaning up

---
+++ ClassFiles/Python201/VirtualEnviromentsPackages/LearnByExploring.py
@@ -0,0 +1,50 @@
+>>> name = "Python 201"
+>>> name.endswith("201")
+True
+>>> name. 
+┌──────────────────────────────────────────────────┐
+│ capitalize   casefold     center                 │
+│ count        encode       endswith               │
+│ expandtabs   find         format                 │
+│ format_map   index        isalnum                │
+│ isalpha      isascii      isdecimal              │
+│ isdigit      isidentifier islower                │
+│ isnumeric    isprintable  isspace                │
+│ istitle      isupper      join                   │
+│ ljust        lower        lstrip                 │
+│ maketrans    partition    replace                │
+│ rfind        rindex       rjust                  │
+│ rpartition   rsplit       rstrip                 │
+│ split        splitlines   startswith             │
+│ strip        swapcase     title                  │
+│ translate    upper        zfill   
+
+>>> 
+>>> words = name.split(" ")
+>>> words
+['Python', '201']
+
+>>> words
+['Python', '201']
+>>> words.reverse
+<built-in method reverse of list object at 0x7fb8e405ac08>
+>>> words.reverse()
+>>> words
+['201', 'Python']
+>>> course = ' '.join (words)
+>>> course
+'201 Python'
+>>> 
+
+>>> words.pop()
+'Python'
+
+>>> words.pop()
+'201'
+>>> words
+
+>>> {}.
+┌──────────────────────────────────────────────────┐
+│ clear      copy       fromkeys   get             │
+│ items      keys       pop        popitem         │
+│ setdefault update     values   
